aa3d5a4be81199797c73ae3445756f67489c7f3d
Stephen Samonte Tan <30998408+stephensamonte@users.noreply.github.com>
2019-01-19 00:57:07 -0500

    References & Keyboard & Mouse Input
    Changes:
    - Added Reference folder
    - Added Keyboard input function
    - Added Mouse input function

--- Automation-App/MainPage.xaml.cs
+++ Automation-App/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Input.Preview.Injection;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -34,6 +37,45 @@ namespace Automation_App
             Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, Jedi Master!");
             mediaElement.SetSource(stream, stream.ContentType);
             mediaElement.Play();
+
+            await Task.Delay(5000);
+            // MoveMouse();
+            TypeText("HelloJedi"); 
+            //String currentDate = GetDate(); 
+            //TypeText(currentDate);
+        }
+
+        // move the mouse to a location on the screen 
+        private void MoveMouse()
+        {
+            var info = new InjectedInputMouseInfo();
+            info.MouseOptions = InjectedInputMouseOptions.Move;
+            // move the mouse down 100 points
+            info.DeltaY = 100;
+
+            InputInjector inputInjector = InputInjector.TryCreate();
+            inputInjector.InjectMouseInput(new[] { info });
+        }
+
+        private async void TypeText(String input)
+        {
+            //TypingTarget.Focus(FocusState.Programmatic);
+            await Task.Delay(100); //we must yield the UI thread so that focus can be acquired
+
+            InputInjector inputInjector = InputInjector.TryCreate();
+            foreach (var letter in input)
+            {
+                var info = new InjectedInputKeyboardInfo();
+                info.VirtualKey = (ushort)((VirtualKey)Enum.Parse(typeof(VirtualKey), letter.ToString(), true));
+                inputInjector.InjectKeyboardInput(new[] { info });
+                await Task.Delay(100);
+            }
+        }
+
+        private String GetDate (){
+
+            return DateTime.Now.ToString("M/d/yyyy");
+
         }
     }
 }
